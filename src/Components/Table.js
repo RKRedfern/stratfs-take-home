0e2b7fc30c7692d5390ef5401af70893a36c361f
RKRedfern <rkredfern@gmail.com>
2021-07-26 13:31:52 -0400

    fetch

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -3,6 +3,19 @@ import AddAccountForm from './AddAccountForm'
 import RowComponent from './RowComponent'
 
 const Table = () => {
+
+    const [tableData, setTableData] = useState({accounts: []})
+
+    useEffect(() => {
+        fetchData()
+    })
+
+    const fetchData = () => {
+        fetch('https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json')
+        .then(r => r.json())
+        .then(r => setTableData(r))
+    }
+
     return(
         <div>
             <h1> This is the table </h1>
